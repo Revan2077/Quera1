46e10bd4f81a0d39cc4b44278a36335dd573ddf5
Reza <r.shirinzadeh@gmail.com>
2024-08-23 04:10:17 +0330

    Create dice.py

---
+++ dice.py
@@ -0,0 +1,16 @@
+def opposite(num):
+    if num == 1:
+        return 6
+    if num == 2:
+        return 5    
+    if num == 3:
+        return 4    
+    if num == 4:
+        return 3    
+    if num == 5:
+        return 2    
+    if num == 6:
+        return 1
+
+n = int(input())
+print(opposite(n))
